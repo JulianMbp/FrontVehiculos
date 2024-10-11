a0c71de1743a6d08f0a8b8c922fe1d8124812b50
julian <julilito09@gmail.com>
2024-10-11 11:30:50 -0500

    agregaa vista

--- src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
+++ src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
 import { Vehiculo } from '../vehiculo';
 import { VehiculosService } from '../vehiculos.service';
+import { Router, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-listar-vehiculos',
   standalone: true,
-  imports: [NgFor, RouterModule ],
+  imports: [NgFor, RouterModule],
   templateUrl: './listar-vehiculos.component.html',
   styleUrl: './listar-vehiculos.component.css'
 })
 export class ListarVehiculosComponent implements OnInit {
-  Vehiculos: Array<Vehiculo> = new Array<Vehiculo>();
-  constructor(private RouterPath: Router, private vehiculosService: VehiculosService){
-
+  Vehiculos: Array<Vehiculo> = [];
+  constructor(private routerPath: Router, private vehiculosService: VehiculosService) {
+    
   }
   ngOnInit(): void {
+    this.obtenerVehiculos();
   }
+  obtenerVehiculos() {
+    this.vehiculosService.obtenerVehiculos().subscribe(vs => {
+      this.Vehiculos = vs;
+      console.log(this.Vehiculos);
+    });
+  }
+onEditarNavigate(id: number) {
+this.routerPath.navigate([`/vehiculos/editar/${id}`]);
+}
 }
