44504f38cdca9edff77900f4cc2a361b159110b3
julian <julilito09@gmail.com>
2024-10-18 11:54:26 -0500

    agregando

---
+++ src/app/vehiculos/crear-vehiculos/crear-vehiculos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Vehiculo } from '../vehiculo';
+import { VehiculosService } from '../vehiculos.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crear-vehiculos',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './crear-vehiculos.component.html',
+  styleUrl: './crear-vehiculos.component.css'
+})
+export class CrearVehiculosComponent implements OnInit {
+  vehiculoForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private vehiculosService: VehiculosService) {}
+
+  ngOnInit()  {
+      this.vehiculoForm = this.formBuilder.group({
+        placa: ['',[Validators.required, Validators.minLength(6)]],
+        marca: ['',[Validators.required, Validators.minLength(2)]],
+        color_vehiculo: ['',[Validators.required, Validators.minLength(2)]],
+        modelo: ['',[Validators.required, Validators.minLength(2)]],
+      })
+  }
+  crearVehiculo(vehiculo:Vehiculo) {
+    this.vehiculosService.crearVehiculos(vehiculo).subscribe(
+      (vehiculoCreado) => {
+        alert('Vehiculo Creado con exito!');
+        this.vehiculoForm.reset();
+      }
+    );
+  }
+}
--- src/app/vehiculos/vehiculos.service.ts
+++ src/app/vehiculos/vehiculos.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Vehiculo } from './vehiculo';
 
-const API_URL = 'http://localhost:8000/api/vehicle/list';
+const API_URL_LIST = 'http://localhost:8000/api/vehicle/list';
+const API_URL_CREATE = 'http://localhost:8000/api/vehicle/crear-vehiculo';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,15 @@ export class VehiculosService {
   
   constructor(private http: HttpClient) {}
   
+  //obtener todos los vehiculos
   obtenerVehiculos(): Observable<Vehiculo[]> {
-    return this.http.get<Vehiculo[]>(API_URL);
+    return this.http.get<Vehiculo[]>(API_URL_LIST);
+  }
+  //crear un nuevo vehiculo
+  crearVehiculos(vehiculo: Vehiculo): Observable<Vehiculo> {
+    return this.http.post<Vehiculo>(API_URL_CREATE , Vehiculo);
+  }
+  obtenerVehiculo(id: number): Observable<Vehiculo> {
+    return this.http.get<Vehiculo>(`${API_URL_LIST}/${id}`);
   }
 }
